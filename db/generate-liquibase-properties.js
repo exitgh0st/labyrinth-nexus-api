e037b56c2b6394b085b0b44fa9f65c253fb16cba
exitgh0st <dev.exitgh0st@gmail.com>
2025-10-23 11:43:44 +0800

    U[date environment file name

--- db/generate-liquibase-properties.js
+++ db/generate-liquibase-properties.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 const environment = process.env.NODE_ENV || "development";
-const envFilePath = path.resolve(__dirname, `../.env.${environment}`); // Adjusted path to .env file
+const envFilePath = path.resolve(__dirname, `../.env.${environment}.local`); // Adjusted path to .env file
 dotenv.config({path: envFilePath });
 
 const liquibaseProperties = `--url="${process.env.JDBC_DATABASE_URL}" --username="${process.env.DB_USER}" --password="${process.env.DB_PASSWORD}" --changeLogFile=db.changelog-master.xml`;
