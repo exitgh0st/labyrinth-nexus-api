b0af14f128f213bc92fd9b87f7f38b3f0c83a616
exitgh0st <dev.exitgh0st@gmail.com>
2025-10-23 11:45:53 +0800

    Update create-user.dto.ts

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsString, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsString()
+  password_hash: string;
+
+  @IsString()
+  role: string;
+
+  @IsBoolean()
+  is_active?: boolean;
+}
