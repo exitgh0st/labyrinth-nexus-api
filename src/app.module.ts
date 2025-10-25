b936cecc949c7d6d26826dbacf8973d9890fea8b
exitgh0st <dev.exitgh0st@gmail.com>
2025-10-25 14:42:08 +0800

    Add config module and auth guard to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { SessionModule } from './session/session.module';
+import { PrismaService } from './prisma.service';
+import { APP_GUARD } from '@nestjs/core';
+import { BearerAuthGuard } from './auth/guards/bearer-auth.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService available everywhere
+      envFilePath: ['.env.development.local'], // Load multiple env files
+      cache: true, // Cache environment variables for performance
+    }),
+    AuthModule, UserModule, SessionModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: BearerAuthGuard, // Apply bearer auth globally
+    }
+  ],
 })
 export class AppModule {}
