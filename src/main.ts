76806ffb4c487f720e7b2425e5ea64749e41006d
exitgh0st <dev.exitgh0st@gmail.com>
2025-10-25 14:46:17 +0800

    Update main.ts to use config service

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,22 @@ import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalFilters(new PrismaExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
 
   app.use(cookieParser());
-    
-    // CORS configuration
-    app.enableCors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-        credentials: true, // Important: allow cookies
-    });
+
+  // CORS configuration
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL', 'http://localhost:3000'),
+    credentials: true, // Important: allow cookies
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
