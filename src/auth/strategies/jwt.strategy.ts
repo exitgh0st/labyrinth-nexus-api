b18805e7416dc87aed3dc829a0a2e7fc9dbcc72b
exitgh0st <dev.exitgh0st@gmail.com>
2025-10-27 15:59:57 +0800

    Update validate function for strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -24,7 +24,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any): Promise<SafeUser> {
-    const user = await this.authService.validateUser(token);
+    const user = await this.authService.validateUserById(payload.sub);
 
     if (!user) {
       throw new UnauthorizedException('Invalid or expired token');
