8c1483e0980189ae4057951257ec2cc17daf53d0
exitgh0st <dev.exitgh0st@gmail.com>
2025-10-27 16:19:24 +0800

    Update safe session select

--- src/session/selects/safe-session.select.ts
+++ src/session/selects/safe-session.select.ts
@@ -4,14 +4,16 @@ import { safeUserSelect } from "src/user/selects/safe-user.select";
 export const safeSessionSelect = {
     id: true,
     user_id: true,
-    token: false,
-    refresh_token: false,
+    session_id: true,
+    refresh_token_hash: false,
     ip_address: true,
     user_agent: true,
     expires_at: true,
     created_at: true,
     last_used_at: true,
     is_revoked: true,
+    previous_session_id: true,
+    updated_at: true,
     user: {
         select: safeUserSelect
     },
