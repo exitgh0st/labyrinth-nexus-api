c05471f98655d642f5f2e350a5c3170379a05ed4
exitgh0st <dev.exitgh0st@gmail.com>
2025-11-20 15:42:44 +0800

    Add winston logger and update validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,27 @@ import { PrismaExceptionFilter } from './shared/filters/prisma-exception.filter'
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true, // Buffer logs until logger is ready
+  });
+  
+  // Use Winston logger from the app context
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  
   const configService = app.get(ConfigService);
 
   app.useGlobalFilters(new PrismaExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
   app.setGlobalPrefix('api');
 
   app.use(cookieParser());
 
-  // CORS configuration
   app.enableCors({
     origin: configService.get<string>('FRONTEND_URL', 'http://localhost:4200'),
-    credentials: true, // Important: allow cookies
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
